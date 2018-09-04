5bb114903b3f4d0ca9631456ddede7f6944c61be
Swapnil Rawat <s.rawat@easternenterprise.com>
2018-09-04 17:17:48 +0530

    On remove search text goelocation restaurant are getting loaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Geolocation } from '@ionic-native/geolocation';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+      HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,6 +37,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+      Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Geolocation} from '@ionic-native/geolocation';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    lat: any;
+    long: any;
+    restList: any;
+    userKey = '';
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, private geolocation: Geolocation, private httpClient: HttpClient) {
+        this.geolocation.getCurrentPosition().then((resp) => {
+            this.lat = resp.coords.latitude;
+            this.long = resp.coords.longitude;
+            console.log(this.lat, this.long);
+            this.getRestaurantListByLocation();
 
-  }
+        }).catch((error) => {
+            console.log('Error getting location', error);
+        });
+
+    }
+
+    getRestaurantListByLocation() {
+        this.httpClient.get(`https://developers.zomato.com/api/v2.1/geocode?lat=${this.lat}&lon=${this.long}`, this.getOptions()).subscribe(data => {
+            this.restList = data.nearby_restaurants;
+        });
+    }
+
+    getRestaurantList(searchText) {
+        if (searchText.trim() !== '') {
+            this.httpClient.get(`https://developers.zomato.com/api/v2.1/search?q=${searchText}`, this.getOptions()).subscribe(data => {
+                this.restList = data.restaurants;
+            });
+        } else {
+            this.getRestaurantListByLocation();
+        }
+    }
+
+    private getOptions() {
+        const headers = new HttpHeaders({
+            'user-key': this.userKey
+        });
+        return {headers: headers};
+    }
 
 }
